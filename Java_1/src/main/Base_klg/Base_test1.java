2e0373a31241cc8309979d647e133abfca43cf79
nvd11 <nvd11@163.com>
2013-12-28 06:08:33 +0000

    learn jdb

--- Java_1/src/main/Base_klg/Base_test1.java
+++ Java_1/src/main/Base_klg/Base_test1.java
@@ -3,6 +3,7 @@ package Base_kng;
 public class Base_test1{
 	public void test_output1(){
 		int i = 100;
-		System.out.printf("%%x is %x, %#x is %#x\n");
+		System.out.printf("%%x is %x, %%#x is %#x\n", i,i);
+		System.out.printf("%%d is %d\n", i);
 	}
 }
--- Java_1/src/main/Enter_1.java
+++ Java_1/src/main/Enter_1.java
@@ -3,9 +3,14 @@ import Base_kng.*;
 public class Enter_1{
 	public static void main(String[] args){
 		System.out.printf("hello ant, it's the my meeting with ant!\n");
-
+		test_out_put();
 		
 	}
 
+
+	private static void test_out_put(){
+		Base_test1 bt1 = new Base_test1();
+		bt1.test_output1();
+	}
 
 }

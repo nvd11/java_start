c267ec863c2f5db541bb07038ebc0c89ba3cb08e
nvd11 <nvd11@163.com>
2015-01-18 19:21:49 +0800

    TCP Start

---
+++ Junit_1/src/cc/A1.java
@@ -0,0 +1,7 @@
+package cc;
+
+public class A1 {
+	public int add(int x, int y){
+		return x + y;
+	}
+}
---
+++ Junit_1/src/text/ATest.java
@@ -0,0 +1,16 @@
+package text;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import cc.A1;
+public class ATest {
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+		int z = new A1().add(3,5);
+		assertEquals(8,z);
+	}
+
+}

cabd610a5deef294fb46fa7a7e47e4336ab7c8e4
nvd11 <nvd11@163.com>
2015-01-18 19:43:48 +0800

    ...

--- Interface_Kng/src/TCPChatClient.java
+++
@@ -1,6 +0,0 @@
-public class TCPChatClient{
-    public static void main(String[] args){
-        System.out.printf("Hello\n");
-        TCPChatClientForm.newInstance();
-    }
-}
--- Interface_Kng/src/TCPChatServer.java
+++
@@ -1,5 +0,0 @@
-public class TCPChatServer{
-    public static void main(String[] args){
-        System.out.printf("Hello i am server\n");
-    }
-}

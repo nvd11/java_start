c8257012964a537cfaf105eeaee3281886f27a9c
nvd11 <nvd11@163.com>
2017-10-15 23:40:28 +0800

    gfiles: log4j implemented!

--- gfiles/src/main/java/com/home/controller/UserController.java
+++ gfiles/src/main/java/com/home/controller/UserController.java
@@ -4,15 +4,22 @@ import com.home.model.GFileUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 
 /**
  * Created by gateman on 17-10-15.
  */
 @Controller
 public class UserController {
+
+	public static Log logging = LogFactory.getLog(UserController.class);
+
     @RequestMapping("/TestUser")
 	public @ResponseBody GFileUser hello(){
 		System.out.println("hello");
+		logging.info("Test for commons logging!");
 		GFileUser user = new GFileUser(1, "jack");
 		return user;
 	}

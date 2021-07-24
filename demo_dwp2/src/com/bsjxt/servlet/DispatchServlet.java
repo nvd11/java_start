a5c5550912adcccb9e4efc8d23953fc9c50d0bbf
nvd11 <nvd11@163.com>
2021-07-24 23:41:31 +0800

    implemented front design pattern

--- demo_dwp2/src/com/bsjxt/servlet/DispatchServlet.java
+++ demo_dwp2/src/com/bsjxt/servlet/DispatchServlet.java
@@ -6,8 +6,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 /*
  *  
- *  "/" only pass *.jsp but not *.html
- * 
+ *  "/" only pass *.jsp but not *.html/ can handle jsp file
+ *  "/*"  cannot handel jsp file
+ *  
+ *  front design pattern
  */
 
 @WebServlet("/")
@@ -15,6 +17,40 @@ public class DispatchServlet extends HttpServlet {
 	
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse rep) {
+		String ctrl_para = req.getParameter("control");
 		System.out.println("executing controller");
+		System.out.println(ctrl_para);
+		
+		if (ctrl_para.equals("demo1")){
+			demo1();
+		}
+			
+		if (ctrl_para.equals("demo2")){
+			demo2();
+		}
+		
+		if (ctrl_para.equals("demo3")){
+			demo3();
+		}
+
+		if (ctrl_para.equals("demo4")){
+			demo4();
+		}
+	}
+	
+	private void demo1(){
+		System.out.println("demo1 is called");
+	}
+	
+	private void demo2(){
+		System.out.println("demo2 is called");
+	}
+	
+	private void demo3(){
+		System.out.println("demo3 is called");
+	}
+	
+	private void demo4(){
+		System.out.println("demo4 is called");
 	}
 }
